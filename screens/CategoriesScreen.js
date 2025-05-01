523e15e54adbe16c3c79333b356e55b72705790d
asnganje <abd.nganje@gmail.com>
2025-05-01 09:42:49 +0300

    use navigation inside categories screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,14 +2,23 @@ import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-const CategoriesScreen = () => {
+const CategoriesScreen = ({navigation}) => {
   return(
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item)=>item.id}
       renderItem={(itemData)=> {
+        const pressHandler = () => {
+          navigation.navigate('MealsOverview', {
+            categoryId:itemData.item.id,
+          })
+        }
         return(
-          <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+          <CategoryGridTile
+            onPress={pressHandler}
+            title={itemData.item.title}
+            color={itemData.item.color}
+          />
         )
       }}
       numColumns={2}
