ccb47cde1a8e9c1b522cf6360a7c8d1c124576ca
asnganje <abd.nganje@gmail.com>
2025-05-01 09:42:14 +0300

    add logic for handling pressing

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,9 +1,10 @@
 import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
-const CategoryGridTile = ({title, color}) => {
+const CategoryGridTile = ({title, color, onPress}) => {
   return (
     <View style={[styles.gridItem, {backgroundColor:color}]}>
       <Pressable
+        onPress={onPress}
         android_ripple={{color:"#ccc"}}
         style={({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}
       >
