fb582418f120a41a4fb6c9d7af523e187c6f8929
asnganje <abd.nganje@gmail.com>
2025-05-01 08:24:17 +0300

    add main categories screen

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.rootScreen}>
+        <StatusBar style="light"/>
+        <CategoriesScreen />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  rootScreen:{
+    flex:1,
+    marginVertical:30
+  }
+})
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const CategoriesScreen = () => {
+  return(
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item)=>item.id}
+      renderItem={(itemData)=> {
+        return(
+          <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+        )
+      }}
+      numColumns={2}
+      />
+  )
+}
+
+export default CategoriesScreen;
